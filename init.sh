7514c64182f30a249a1af57116e74dcc52682961
liukan <liukan@126.com>
2018-06-01 06:44:03 +0000

    docker-compose

--- init.sh
+++ init.sh
@@ -18,7 +18,6 @@ cd alpine-nginx-php-fpm-for-transmisson/ssl/
 if hash docker-compose 2>/dev/null; then
 	echo "init done"
 else
-	sudo curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-	sudo chmod +x /usr/local/bin/docker-compose
+	sudo apt install -y python-pip ; sudo pip install docker-compose
 	echo "init done"
 fi
